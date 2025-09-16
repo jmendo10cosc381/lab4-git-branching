5f2075990fd2ba232b1bc0ff1a64573238c8b459
Julia Mendoza <231963277+jmendo10cosc381@users.noreply.github.com>
2025-09-16 13:51:50 -0400

    Added to hero story pt 1

--- story.py
+++ story.py
@@ -9,10 +9,12 @@ def intro():
         center_path()
     else:
         print("You stand still, unsure what to do. The forest swallows you.")
-
+#HERO PATH
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("You pick you the glowing sword and the sword and decide to become an adventure")
 
+#VILLAIN PATH
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
 #DONT FORGET TO WRITE STORY

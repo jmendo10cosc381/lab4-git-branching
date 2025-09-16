aeb3fca954b4e7bb3540f4c34d5f8f987e881715
Julia Mendoza <231963277+jmendo10cosc381@users.noreply.github.com>
2025-09-16 13:37:19 -0400

    Added a center path.

--- story.py
+++ story.py
@@ -5,6 +5,8 @@ def intro():
         left_path()
     elif choice == "right":
         right_path()
+    elif choice == "center":
+        center_path()
     else:
         print("You stand still, unsure what to do. The forest swallows you.")
 
@@ -13,6 +15,9 @@ def left_path():
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
+#DONT FORGET TO WRITE STORY
+def center_path():
+    print("I will make up the story at the end!!!")
 
 if __name__ == "__main__":
     intro()
